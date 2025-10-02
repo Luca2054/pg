0fc4b1f77b3688fab809f4f8a6fb6aa1a6fe350a
Luca2054 <hintel01@jcu.cz>
2025-10-02 08:51:10 +0000

    1. cvičení

---
+++ first.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    vstup = int(input("Zadej cislo: "))
+
+        if vstup > 10:
+            print("vetsi nez 10")   
+        elif vstup < 10:
+            print("menši nez 10")    
+        else vetup = 10:¨
+            print("rovno 10")         
+        
+
+if __name__ == "__name__":
+    main()
