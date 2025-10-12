0a00d0ed316708f93ade93e8f444dada999bf913
Luca2054 <hintel01@jcu.cz>
2025-10-12 18:03:26 +0000

    second

---
+++ prednaska.py
@@ -0,0 +1,15 @@
+
+
+if__name__=="__main__"
+    hodnota = input("Zadej číslo:")
+    Hodnota - int(hodnota)
+
+
+    operator = input ("Zadej operator (+,-,*,/):")
+
+    hodnota2 = input("Zadej číslo: ")
+    hodnota2 = int(hodnota2)
+
+    print(f"Operace: {hodnota} {operator} {hodnota2}")
+
+    vysledek = None
---
+++ scond.py/second.py
@@ -0,0 +1,9 @@
+def cislo_text ():
+    cislo = int(input ("zadej číslo:"))
+    
+    if cislo == 0:
+     print("nula")
+    elif cislo == 1:
+      print("jedna")
+    else:
+        print("jiné číslo")
